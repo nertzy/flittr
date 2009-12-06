507e8d95481ee483dc9a2f0261c039d010787797
Grant Hutchins <nertzy@gmail.com>
2009-12-06 05:03:59 -0600

    Oops, turn caching back on.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
-  # caches_action :show, :expires_in => 10.minutes
+  caches_action :show, :expires_in => 10.minutes
   def show
-    # expires_in 10.minutes, :public => true
+    expires_in 10.minutes, :public => true
     @user = User.new(:screen_name => params[:id])
   end
 end
