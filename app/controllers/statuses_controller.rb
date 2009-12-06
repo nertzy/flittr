c9030f5b132b5e71b207fc5923ce44321e9528be
Grant Hutchins <nertzy@gmail.com>
2009-12-06 00:47:55 -0600

    Add action caching and HTTP caching to user and status pages.

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -1,7 +1,7 @@
 class StatusesController < ApplicationController
-
+  caches_action :show, :expires_in => 2.days
   def show
+    expires_in 2.days, :public => true
     @status = Status.new(:id => params[:id])
   end
-
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
+  caches_action :show, :expires_in => 10.minutes
   def show
+    expires_in 10.minutes, :public => true
     @user = User.new(:name => params[:id])
   end
-
 end
