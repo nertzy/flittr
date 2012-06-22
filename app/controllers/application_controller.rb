3b8d1c222e39b9e70b411f24728e5935397b74f9
Grant Hutchins <nertzy@gmail.com>
2012-06-21 20:55:22 -0400

    Protect from forgery

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,16 +3,16 @@
 
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
-  # protect_from_forgery # See ActionController::RequestForgeryProtection for details
+  protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
 
   rescue_from Grackle::TwitterError, :with => :twitter_error
   rescue_from User::NotAuthorized, :with => :not_authorized
-  
+
   private
-  
+
   def twitter_error
     render '/error/twitter', :layout => 'application', :status => 500
   end
@@ -20,5 +20,5 @@ class ApplicationController < ActionController::Base
   def not_authorized
     render '/error/not_authorized', :layout => 'application', :status => 500
   end
-  
+
 end
