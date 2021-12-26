b3d735b33ff01f519e1629610be9a386a3906092
Grant Hutchins <git@nertzy.com>
2021-12-26 13:45:57 -0600

    Fix outdated test

--- test/functional/statuses_controller_test.rb
+++ test/functional/statuses_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class StatusesControllerTest < ActionController::TestCase
   test 'show' do
-    get :show, :id => 6361636644
+    get :show, params: { :id => 6361636644 }
     assert_response :success
   end
 end
